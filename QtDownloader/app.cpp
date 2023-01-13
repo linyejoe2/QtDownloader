2e3030bd2a728497cf15f10d6f76504d0ba14d0b
Randy Lin <linyejoe2@gmail.com>
2023-01-13 16:56:37 +0800

    Create project
    - Create basic project structure
    - Add main window and widgets
    - Configure build settings

---
+++ QtDownloader/app.cpp
@@ -0,0 +1,15 @@
+﻿#include "app.h"
+#include "ui_app.h"
+
+App::App(QWidget *parent)
+	: QMainWindow(parent)
+	, ui(new Ui::App)
+{
+	ui->setupUi(this);
+	this->setWindowTitle(QStringLiteral("QtDownloader"));
+}
+
+App::~App()
+{
+	delete ui;
+}
---
+++ QtDownloader/app.h
@@ -0,0 +1,21 @@
+#ifndef APP_H
+#define APP_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class App; }
+QT_END_NAMESPACE
+
+class App : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	App(QWidget *parent = nullptr);
+	~App();
+
+private:
+	Ui::App *ui;
+};
+#endif // APP_H
